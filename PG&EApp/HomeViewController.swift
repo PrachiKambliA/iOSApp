d536e8209fae5a36fac9c83eefd119f99011f357
Luis Valencia <lvalencia@csumb.edu>
2020-05-29 12:40:24 -0500

    added first part of material flow

--- PG&EApp/HomeViewController.swift
+++ PG&EApp/HomeViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 
 class HomeViewController: UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         navigationController?.setNavigationBarHidden(true, animated: animated)
